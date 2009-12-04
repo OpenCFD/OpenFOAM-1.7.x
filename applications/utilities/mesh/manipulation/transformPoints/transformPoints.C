6ab91f6db76a79ef22fa3bee78eada3120df4214
mattijs <mattijs>
2009-12-04 19:06:32 +0000

    added region option

--- applications/utilities/mesh/manipulation/transformPoints/transformPoints.C
+++ applications/utilities/mesh/manipulation/transformPoints/transformPoints.C
@@ -92,9 +92,9 @@ void readAndRotateFields
 }
 
 
-void rotateFields(const Time& runTime, const tensor& T)
+void rotateFields(const argList& args, const Time& runTime, const tensor& T)
 {
-#   include "createMesh.H"
+#   include "createNamedMesh.H"
 
     // Read objects in time directory
     IOobjectList objects(mesh, runTime.timeName());
@@ -141,6 +141,7 @@ void rotateFields(const Time& runTime, const tensor& T)
 
 int main(int argc, char *argv[])
 {
+#   include "addRegionOption.H"
     argList::validOptions.insert("translate", "vector");
     argList::validOptions.insert("rotate", "(vector vector)");
     argList::validOptions.insert("rollPitchYaw", "(roll pitch yaw)");
@@ -151,13 +152,25 @@ int main(int argc, char *argv[])
 #   include "setRootCase.H"
 #   include "createTime.H"
 
+    word regionName = polyMesh::defaultRegion;
+    fileName meshDir;
+
+    if (args.optionReadIfPresent("region", regionName))
+    {
+        meshDir = regionName/polyMesh::meshSubDir;
+    }
+    else
+    {
+        meshDir = polyMesh::meshSubDir;
+    }
+
     pointIOField points
     (
         IOobject
         (
             "points",
-            runTime.findInstance(polyMesh::meshSubDir, "points"),
-            polyMesh::meshSubDir,
+            runTime.findInstance(meshDir, "points"),
+            meshDir,
             runTime,
             IOobject::MUST_READ,
             IOobject::NO_WRITE,
@@ -196,7 +209,7 @@ int main(int argc, char *argv[])
 
         if (args.optionFound("rotateFields"))
         {
-            rotateFields(runTime, T);
+            rotateFields(args, runTime, T);
         }
     }
     else if (args.optionFound("rollPitchYaw"))
@@ -219,7 +232,7 @@ int main(int argc, char *argv[])
 
         if (args.optionFound("rotateFields"))
         {
-            rotateFields(runTime, R.R());
+            rotateFields(args, runTime, R.R());
         }
     }
     else if (args.optionFound("yawPitchRoll"))
@@ -248,7 +261,7 @@ int main(int argc, char *argv[])
 
         if (args.optionFound("rotateFields"))
         {
-            rotateFields(runTime, R.R());
+            rotateFields(args, runTime, R.R());
         }
     }
 
