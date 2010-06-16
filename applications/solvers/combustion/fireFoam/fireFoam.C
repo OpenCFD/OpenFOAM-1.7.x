371b644c23c8606ce95b969800edf936a05e20be
henry <Henry Weller h.weller@opencfd.co.uk>
2010-06-16 12:26:16 +0100

    fireFoam: converted into a p_rgh solver.

--- applications/solvers/combustion/fireFoam/fireFoam.C
+++ applications/solvers/combustion/fireFoam/fireFoam.C
@@ -78,7 +78,7 @@ int main(int argc, char *argv[])
             // --- PISO loop
             for (int corr=0; corr<nCorr; corr++)
             {
-                #include "pEqn.H"
+                #include "p_rghEqn.H"
             }
         }
 
