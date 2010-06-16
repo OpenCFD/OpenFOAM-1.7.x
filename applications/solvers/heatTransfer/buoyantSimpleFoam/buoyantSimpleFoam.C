06cf2aa16b185133ca645b7446e82093658d0d6a
henry <Henry Weller h.weller@opencfd.co.uk>
2010-06-16 18:38:54 +0100

    buoyantSimpleFoam: Now uses p_rgh.
    Also added another tutorial case.

--- applications/solvers/heatTransfer/buoyantSimpleFoam/buoyantSimpleFoam.C
+++ applications/solvers/heatTransfer/buoyantSimpleFoam/buoyantSimpleFoam.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 1991-2010 OpenCFD Ltd.
+    \\  /    A nd           | Copyright (C) 1991-2009 OpenCFD Ltd.
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -56,7 +56,7 @@ int main(int argc, char *argv[])
         #include "readSIMPLEControls.H"
         #include "initConvergenceCheck.H"
 
-        p.storePrevIter();
+        p_rgh.storePrevIter();
         rho.storePrevIter();
 
         // Pressure-velocity SIMPLE corrector
