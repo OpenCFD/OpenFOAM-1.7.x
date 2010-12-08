5ef3112d638299e504177f2cf6dc83d05a3e4b93
mattijs <mattijs>
2010-12-08 11:38:42 +0000

    STYLE: removed some orphaned files

--- applications/solvers/combustion/PDRFoam/PDRFoamAutoRefine.C
+++ applications/solvers/combustion/PDRFoam/PDRFoamAutoRefine.C
@@ -26,8 +26,8 @@ Application
     PDRFoam
 
 Description
-    Compressible premixed/partially-premixed combustion solver with turbulence
-    modelling.
+    Solver for compressible premixed/partially-premixed combustion with
+    turbulence modelling.
 
     Combusting RANS code using the b-Xi two-equation model.
     Xi may be obtained by either the solution of the Xi transport
@@ -36,7 +36,7 @@ Description
     to be appropriate by comparison with the results from the
     spectral model.
 
-    Strain effects are encorporated directly into the Xi equation
+    Strain effects are incorporated directly into the Xi equation
     but not in the algebraic approximation.  Further work need to be
     done on this issue, particularly regarding the enhanced removal rate
     caused by flame compression.  Analysis using results of the spectral
@@ -79,9 +79,9 @@ int main(int argc, char *argv[])
     #include "readCombustionProperties.H"
     #include "readGravitationalAcceleration.H"
     #include "createFields.H"
-    #include "readPISOControls.H"
     #include "initContinuityErrs.H"
     #include "readTimeControls.H"
+    #include "CourantNo.H"
     #include "setInitialDeltaT.H"
 
     scalar StCoNum = 0.0;
@@ -95,12 +95,7 @@ int main(int argc, char *argv[])
         #include "readTimeControls.H"
         #include "readPISOControls.H"
         #include "CourantNo.H"
-
         #include "setDeltaT.H"
-
-        runTime++;
-
-        Info<< "\n\nTime = " << runTime.timeName() << endl;
 
         // Indicators for refinement. Note: before runTime++
         // only for postprocessing reasons.
@@ -112,6 +107,11 @@ int main(int argc, char *argv[])
         );
         normalisedGradP.writeOpt() = IOobject::AUTO_WRITE;
         tmagGradP.clear();
+
+        runTime++;
+
+        Info<< "\n\nTime = " << runTime.timeName() << endl;
+
 
         bool meshChanged = false;
         {
